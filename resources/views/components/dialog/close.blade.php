ff49eceadfc706d1229010a9ead1aa2bb032e734
Sowry <sowry@rascality.nz>
2024-01-25 13:10:07 +1300

    Change icon to be xmark

--- resources/views/components/dialog/close.blade.php
+++ resources/views/components/dialog/close.blade.php
@@ -3,7 +3,7 @@
     <span class="sr-only">{{ __('crud.delete_modal.close') }}</span>
 </button>@else
 <button autofocus type="button" class="float-right text-xl rounded-full bg-base-300 opacity-80 hover:opacity-100" onclick="this.closest('dialog').close('close')" title="{{ __('crud.delete_modal.close') }}">
-    <x-icon class="fa-regular fa-times"></x-icon>
+    <x-icon class="fa-solid fa-xmark"></x-icon>
     <span class="sr-only">{{ __('crud.delete_modal.close') }}</span>
 </button>
 @endif
